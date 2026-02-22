b1eee7d2fa8cc487a70b7263caa0e314d53c10c6
ToxicPL <toxicpl00@gmail.com>
2026-02-22 23:08:00 +0100

    Add timestamp logging and improve update check in auto-update.sh

--- auto-update.sh
+++ auto-update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Version 1.0 Auto-update script to pull latest upgrades and log the update time
 # Verion 1.1 - Added webhook notification with list of updated packages
+# Version 1.2 - Added check for available updates before patching and logging accordingly
 LOGFILE="/mnt/usb-drv/1tb/log.txt"
 WEBHOOK_URL="https://example.com/webhook" # Replace with your webhook URL
 HOSTNAME=$(hostname)
 NEW_PACKAGES=$(apt-get -s upgrade | awk '/^Inst / {print $2}')
 PKG_LIST=$(echo "$NEW_PACKAGES" | tr '\n' ', ')
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
 
 patching(){
@@ -13,9 +15,17 @@ patching(){
     sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y;
 };
 
-patching;
-TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$TIMESTAMP - System updated successfully" >> "$LOGFILE"
- MESSAGE="$TIMESTAMP - System updated successfully on $HOSTNAME. New/updated packages: $PKG_LIST"
+if [ -z "$NEW_PACKAGES" ]; then
+    echo "$TIMESTAMP - No new packages to update." >> "$LOGFILE"
+    MESSAGE="$TIMESTAMP - No updates available on $HOSTNAME."
         curl -X POST -H 'Content-type: application/json' --data "{\"content\":\"$MESSAGE\"}" \
         "$WEBHOOK_URL"
+
+else
+    patching;
+    echo "$TIMESTAMP - System updated successfully" >> "$LOGFILE"
+    MESSAGE="$TIMESTAMP - System updated successfully on $HOSTNAME. New/updated packages: $PKG_LIST"
+        curl -X POST -H 'Content-type: application/json' --data "{\"content\":\"$MESSAGE\"}" \
+        "$WEBHOOK_URL"
+fi
+
